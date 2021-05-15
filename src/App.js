694975be9a9202c191541d84a7f3f06cb420e24b
Karelin Alexander <a.karelin@i-teco.ru>
2021-05-15 03:26:20 +0300

    Небольшие правки

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import clock from './clock-face.jpg';
+import React from 'react';
+import clock from './clockFace.jpg';
 import './App.css';
 
 function App({hour, min, sec}) {
-    const hourRotate = hour * 360/12 + min * 360/12/60 - 90
-    const minRotate = min * 360/60 + sec * 360/60/60 - 90
+    const hourRotate = hour * 360/12 + min * 360/12/60 - 90;
+    const minRotate = min * 360/60 + sec * 360/60/60 - 90;
     return (
         <div className="App">
             <header className="App-header">
                 <div className="clock">
                     <div className="clock-hour" style={{transform: `rotateZ(${hourRotate}deg)`}} />
                     <div className="clock-minute" style={{transform: `rotateZ(${minRotate}deg)`}} />
-                    <img src={clock} className="App-logo" alt="logo"/>
+                    <img src={clock} alt="clock"/>
                 </div>
             </header>
         </div>
